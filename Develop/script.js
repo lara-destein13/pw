bf09c02ba5d7bf168e72e1ac0a7ecbce604fd005
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-01-06 21:41:48 -0500

    start javascript

---
+++ Develop/script.js
@@ -0,0 +1,16 @@
+function generatePassword() {
+  window.alert("Please enter your password criteria");
+  var length = window.prompt("Please enter the number of characters you would like for your password. (must be between 8 and 128)");
+  console.log("You chose " + length + " characters for your password.");
+}
+
+
+generatePassword();
+
+
+
+
+
+
+
+
