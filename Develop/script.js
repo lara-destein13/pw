329d9a3a7625c99111010a987f79afeae16fa7b7
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-01-06 22:04:36 -0500

    length function

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,30 @@
-function generatePassword() {
-  window.alert("Please enter your password criteria");
+// var passwordLength
+// var characterTypes = [ "!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+// var finalPassword 
+
+
+// function generatePassword() {
+//   window.alert("Please enter your password criteria");
+
+// }
+
+// generatePassword();
+
+
+window.alert("Please enter your password criteria");
+
+function getLength() {
   var length = window.prompt("Please enter the number of characters you would like for your password. (must be between 8 and 128)");
   console.log("You chose " + length + " characters for your password.");
 }
 
+getLength();
 
-generatePassword();
+// var getLength = function() {
+//   window.prompt("Please enter the number of characters you would like for your password. (must be between 8 and 128)");
+// };
 
-
-
+// console.log("You chose " + getLength + " characters for your password.");
 
 
 
