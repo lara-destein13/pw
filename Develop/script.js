bd3053a130506f28ccfe0af82d16a9de2e2b873c
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-02-03 19:44:12 -0500

    length function working

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,11 @@
-
 function startPasswordGenerator() {
   var length = window.prompt("How many characters would you like in your password. (must be between 8 and 128");
-  while (length >= 8 && length <= 128) {
-    window.alert("length suffienct")
+  console.log(length);
+  if (length >= 8 && length <= 128) {
+    window.alert("You chose " + length + " characters for your password.");
+  } else {
+    window.alert("Insufficient character length, please choose a number 8-128");
+    (startPasswordGenerator());
   }
 };
 
@@ -11,32 +14,3 @@ startPasswordGenerator();
 
 
 
-
-
-  // while (length >= 8 && length <= 128) {
-  //   window.alert("You chose " + length + " characters.");
-
-  
-
-
-
-// function charactersToInclude() {
-//   var confirmLower = window.confirm("would you like your password to include lowercase characters?");
-//     if (confirmLower) {
-//       window.alert("abcdefghijklmnopqrstuvwxyz");
-//     } else {
-//       window.alert("no lowercase");      
-//   }
-// }
-
-// var createNewPassword = window.confirm("would you like to create a new password?");
-
-// function endPasswordGenerator() {
-//   if (createNewPassword === true) {
-//     startPasswordGenerator();
-//   } else {
-//     window.alert("Thank you for using password generator");
-//   }
-// }
-
-// endPasswordGenerator();
