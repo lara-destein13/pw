662bc9a8a1f82284019ae10637ce35e9d3050122
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-04-07 19:07:34 -0400

    get button working

--- Develop/script.js
+++ Develop/script.js
@@ -5,9 +5,8 @@ const numeric = ['0','1','2','3','4','5','6','7','8','9']
 
 var button = document.querySelector("button");
 button.addEventListener("click", function() {
-  alert("button clicked");
+  criteria();
 });
-
 
 // function startPasswordGenerator() {
 function criteria () {
@@ -87,6 +86,5 @@ function criteria () {
 
 }
 
-criteria();
     
     
